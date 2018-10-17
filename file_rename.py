792e0e3ec1ab0b22127a1070f62f6f821453b13d
Mingyu Zheng <mzheng@g.hmc.edu>
2018-10-16 20:17:32 -0700

    fixed code and filenames

--- file_rename.py
+++ file_rename.py
@@ -2,8 +2,10 @@ import glob, os
 
 def rename():
     n = 1
-    for filename in glob.glob('grs\*'):
-        os.rename(filename, str(n)+".jpg")
+    print("ran")
+    for filename in glob.glob('grs/*'):
+        print(n)
+        os.rename(filename, "grs/"+str(n)+".jpg")
         n += 1
 
 def main():
